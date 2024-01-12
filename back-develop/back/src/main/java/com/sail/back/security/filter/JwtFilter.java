803d000e69f8d2ca14e5604ee94f264682e1d8f1
IBH0403 <leikra44@gmail.com>
2024-01-12 17:52:35 +0900

    1st week end

--- back-develop/back/src/main/java/com/sail/back/security/filter/JwtFilter.java
+++
@@ -1,4 +0,0 @@
-package com.sail.back.security.filter;
-
-public class JwtFilter {
-}
--- back-develop/back/src/main/java/com/sail/back/user/model/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sail.back.user.model.repository;
-
-import com.sail.back.user.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
